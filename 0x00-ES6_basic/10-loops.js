b95a40fcde1c40684d70871d6b1f5a35ccd448e2
Ngwanatuka <ngwanatuka@gmail.com>
2023-07-25 20:46:18 +0200

    11th task that appends

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-restricted-syntax */
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+
+  for (const value of array) {
+    newArray.push(appendString + value);
+  }
+
+  return newArray;
+}
