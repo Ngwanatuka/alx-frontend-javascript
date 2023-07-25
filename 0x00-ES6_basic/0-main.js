72e1d69892d84c89e634db8523b6d70a6f1094ca
Ngwanatuka <ngwanatuka@gmail.com>
2023-07-25 12:45:25 +0200

    added the first file

---
+++ 0x00-ES6_basic/0-main.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/extensions */
+import { taskFirst, taskNext } from './0-constants.js';
+
+// eslint-disable-next-line no-console
+console.log(`${taskFirst()} ${taskNext()}`);
