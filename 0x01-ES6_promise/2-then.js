e39b9c28404407adab665d2d23d47e1cd352aae7
Ngwanatuka <ngwanatuka@gmail.com>
2023-07-26 22:25:29 +0200

    3rd task  appending three handlers

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+function handleResponseFromAPI(promise) {
+  return promise
+    .then((response) => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch((error) => {
+      console.error(error);
+      return new Error();
+    });
+}
+
+export default handleResponseFromAPI;
