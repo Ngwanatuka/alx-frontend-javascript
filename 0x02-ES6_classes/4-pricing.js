ce36ee72d1e4955bbd07c916a557feda4b9bc162
Ngwanatuka <ngwanatuka@gmail.com>
2023-07-28 04:37:38 +0200

    function to return  amount multiplied by conversion rate

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-underscore-dangle */
+class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getter and setter for amount
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount === 'number') {
+      this._amount = newAmount;
+    } else {
+      throw new TypeError('Amount must be a number');
+    }
+  }
+
+  // Getter and setter for currency
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (newCurrency instanceof Currency) {
+      this._currency = newCurrency;
+    } else {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+  }
+
+  // Method to display full price
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // Static method the price using the conversion rate
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('Amount and conversionRate must be numbers');
+    }
+
+    return amount * conversionRate;
+  }
+}
+
+export default Pricing;
