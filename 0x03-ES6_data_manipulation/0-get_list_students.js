dfb1d3844bdb948e4bbd9949440e2dedbad9bc19
Ngwanatuka <ngwanatuka@gmail.com>
2023-08-01 11:37:34 +0200

    fixed the name of the first id

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,6 @@
 function getListStudents() {
   return [
-    { id: 1, firstName: 'Guilluame', location: 'San Francsisco' },
+    { id: 1, firstName: 'Guillaume', location: 'San Francsisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francsisco' },
   ];
