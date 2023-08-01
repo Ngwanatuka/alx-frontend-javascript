e3ae6bb09de02503223cae12f4a8829720076481
Ngwanatuka <ngwanatuka@gmail.com>
2023-08-01 11:33:10 +0200

    function that returns an array of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guilluame', location: 'San Francsisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francsisco' },
+  ];
+}
+
+export default getListStudents;
