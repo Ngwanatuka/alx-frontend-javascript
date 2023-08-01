67b789ed2a46d2e70eff633a9a56c678ce1cd94e
Ngwanatuka <ngwanatuka@gmail.com>
2023-08-01 13:13:04 +0200

    update student grade by city function

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,15 @@
+function updateStudentGradeByCity(studentList, city, newGrades) {
+  if (!Array.isArray(studentList)) {
+    return [];
+  }
+
+  return studentList
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObject = newGrades.find((grade) => grade.studentId === student.id);
+      const grade = gradeObject ? gradeObject.grade : 'N/A';
+      return { ...student, grade };
+    });
+}
+
+export default updateStudentGradeByCity;
